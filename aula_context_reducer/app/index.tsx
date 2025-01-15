02c582c1028d27c8de30ee50e7f94724302214a7
Andrey Mendonca <andrey.mmendonca@gmail.com>
2025-01-15 15:47:28 -0300

    Aula sobre reducer

--- aula_context_reducer/app/index.tsx
+++ aula_context_reducer/app/index.tsx
@@ -1,9 +1,12 @@
 import { Button, StyleSheet, Text, View } from "react-native";
 import { Area } from "../components/area1";
-import { useContext, useState } from "react";
+import { useContext, useReducer, useState } from "react";
 import { AuthContext } from "../contexts/auth";
+import { ageReducer } from "../reducers/age";
 
 export default function Screen(){
+
+    const [ageReducerState, ageReducerDispatch] = useReducer(ageReducer, 10);
 
     const auth = useContext(AuthContext);
 
@@ -11,8 +14,16 @@ export default function Screen(){
         auth?.setData("Andrey")
     }
 
+    const handleIncreaseAge = () =>{
+        ageReducerDispatch({
+            type: 'INCREASE'
+        })
+    }
+
     return (
         <View style={styles.container}>
+            <Text>Eu tenho {ageReducerState} anos</Text>
+            <Button title="Aumentar idade reducer" onPress={handleIncreaseAge}/>
             <Text>Estou aqui {auth?.data}</Text>
             <Area/>
             <Button title="Alterar nome" onPress={handleClick}/>
