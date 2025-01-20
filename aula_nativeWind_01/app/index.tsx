25ec4ac872d1659230c20aed7419a0ca2b0d7fb5
Andrey Mendonca <andrey.mmendonca@gmail.com>
2025-01-20 14:38:55 -0300

    Conteudos de nativewind

---
+++ aula_nativeWind_01/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Screen(){
+    return (
+        <View>
+            <Text className="text-3xl text-red-500">Ola</Text>
+        </View>
+    )
+}
