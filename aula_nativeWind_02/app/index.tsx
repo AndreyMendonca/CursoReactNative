40b3c3379b7fe42b661a543c238cef94fa009ec4
Andrey Mendonca <andrey.mmendonca@gmail.com>
2025-01-21 17:50:04 -0300

    Conteudo de nativeWind

---
+++ aula_nativeWind_02/app/index.tsx
@@ -0,0 +1,21 @@
+import {useColorScheme} from 'nativewind'
+import { Button, Text, View } from "react-native";
+
+export default function Index() {
+
+    const {colorScheme, setColorScheme} = useColorScheme();
+    
+
+    return (
+        <View className="h-min-screen bg-white dark:bg-black">
+            <Text className="text-personalizada dark:text-white">Texto editado com nativeWind</Text>
+
+            <Text className="text-black dark:text-white">Color scheme {colorScheme}</Text>
+
+            <Button title="DARK" onPress={() => setColorScheme('dark')}/>
+            <Button title="LIGHT" onPress={() => setColorScheme('light')}/>
+            <Button title="SYSTEM" onPress={() => setColorScheme('system')}/>
+
+        </View>
+  );
+}
